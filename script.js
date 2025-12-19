4a221cc687f7620e53858e1a51284c96a56791a0
Amal kacem <116800799+AmalK20@users.noreply.github.com>
2025-12-19 16:41:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Animation simple au scroll
 const elements = document.querySelectorAll(".section");
+const projects = {
+  aerophobia: {
+    title: "Simulation VR – Aérophobie",
+    video: "assets/videos/aerophobia.mp4",
+    desc: "Simulation immersive permettant une exposition progressive à la peur de l’avion."
+  }
+};
 
 window.addEventListener("scroll", () => {
   elements.forEach(el => {
@@ -17,3 +24,13 @@ elements.forEach(el => {
   el.style.transition = "all 0.8s ease";
 });
 
+function openProject(id) {
+  document.getElementById("modal-title").innerText = projects[id].title;
+  document.getElementById("modal-video").src = projects[id].video;
+  document.getElementById("modal-desc").innerText = projects[id].desc;
+  document.getElementById("project-modal").style.display = "block";
+}
+
+function closeProject() {
+  document.getElementById("project-modal").style.display = "none";
+}
